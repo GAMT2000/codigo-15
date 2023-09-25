a35c57de4ade4e1ded806c9c4986d247787e1f41
GAMT2000 <mt2020.va@gmail.com>
2023-09-25 03:08:02 -0500

    vanilla proyect completed

--- vanilla-proyect/main.js
+++ vanilla-proyect/main.js
@@ -1,27 +1,24 @@
 import './style.css'
-import { getGenres } from './movie';
+import { getGenres, getGenreByURL } from './movie';
+
 
 document.querySelector("#app").innerHTML = `
-<div class="p-6 max-w-[60%] m-auto">
+<div class="p-6 lg:max-w-[60%] m-auto">
     <div>
       <h1 class="text-6xl font-bold text-blue-800">Películas</h1>
     </div>
     <hr class="mt-5" />
-    <div class="flex gap-20 mt-10">
+    <div class="flex flex-col md:flex-row gap-20 mt-3 md:mt-10">
       <div class="flex-1">
         <div>
           <h4 class="text-lg font-semibold">Filter Por:</h4>
         </div>
-        <div id="container-genres">
+        <div id="container-genres" class="flex md:block overflow-auto gap-3 whitespace-nowrap">
         </div>
       </div>
       <div>
-        <div class="grid grid-cols-3 gap-20">
-          <div>
-            <img class="w-[200px]" src="https://cdn.apis.cineplanet.com.pe/CDN/media/entity/get/FilmPosterGraphic/HO00001790?referenceScheme=HeadOffice&allowPlaceHolder=true" />
-            <h2 class="font-semibold text-xl mt-2">After: Para Siempre</h2>
-            <p>Drama, 1h 40min, +14.</p>
-          </div>
+        <div id="grid-movies" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
+          
         </div>
       </div>
     </div>
@@ -29,3 +26,4 @@ document.querySelector("#app").innerHTML = `
 `;
 
 getGenres(document.querySelector("#container-genres"))
+getGenreByURL()
--- vanilla-proyect/movie.js
+++ vanilla-proyect/movie.js
@@ -1,15 +1,70 @@
-const urlGenres = "https://moviesminidatabase.p.rapidapi.com/genres/"
+const urlGenres = "https://moviesminidatabase.p.rapidapi.com/genres/";
+const urlMoviesByGenre =
+  "https://moviesminidatabase.p.rapidapi.com/movie/byGen/";
+
+const urlMovieDetail = "https://moviesminidatabase.p.rapidapi.com/movie/id/";
+
+const options = {
+  headers: {
+    "X-RapidAPI-Key": "ca49858e5dmsh97d0e2c9e04d20cp1d6f7ejsn61ba730b1793",
+    "X-RapidAPI-Host": "moviesminidatabase.p.rapidapi.com",
+  },
+};
+
+async function makeRequest(url) {
+  const response = await fetch(url, options)
+  const data = await response.json()
+
+  return data
+}
+
+const getTextGenreByURL = () =>  location.search.split("=")[1];
+
+
+function renderItemList(genre) {
+  const genreFromURL = getTextGenreByURL();
+  const classToElement =
+    genreFromURL === genre
+      ? "text-blue-800 font-semibold"
+      : "hover:text-blue-800 hover:font-semibold";
+
+  return `
+    <p class="my-2 ${classToElement}">
+      <a href="?genre=${genre}" class="cursor-pointer">${genre}</a>
+    </p>
+  `;
+}
 
 export async function getGenres(element) {
-    const response = await fetch(urlGenres, {
-        headers: {
-            "X-RapidAPI-Key": "4cc4c577cdmsh6e25517382b8781p16d967jsn992e2eeb5bdc",
-            "X-RapidAPI-Host": "moviesminidatabase.p.rapidapi.com"
-        }
-    })
+  const data = await makeRequest(urlGenres)
 
-    const data = await response.json()
-    data.results.forEach((item) => {
-        element.innerHTML += `<p class="my-2 hover:text-blue-800 hover:font-semibold"><a class="cursor-pointer">${item.genre}</a></p>`
-    })
+  data.results.forEach(({genre}) => element.innerHTML += renderItemList(genre));
+}
+
+async function renderMovie(movie) {
+  const data = await makeRequest(`${urlMovieDetail}${movie.imdb_id}`, options)
+  const movieData = data.results;
+
+  return `<div>
+    <img class="w-full md:w-[200px] h-[300px] object-cover" src="${movieData.banner}" />
+    <h2 class="font-semibold text-xl mt-2">${movie.title}</h2>
+    <p class="truncate">${movieData.plot}</p>
+  </div>`;
+}
+
+export async function getGenreByURL() {
+  const genre = getTextGenreByURL();
+
+  // caso1: si hay un error
+  if (!genre) return;
+
+  // caso2: cuando no hay error (hacemos la busqueda)
+  // https://moviesminidatabase.p.rapidapi.com/movie/byGen/genre
+  const data = await makeRequest(`${urlMoviesByGenre}${genre}`, options)
+  const containerMovies = document.querySelector("#grid-movies");
+
+  data.results.slice(0, 6).forEach(async (movie) => {
+    const movieData = await renderMovie(movie);
+    containerMovies.innerHTML += movieData;
+  });
 }
