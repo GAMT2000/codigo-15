72b5784463148697b948851c268a096a248f30b5
GAMT2000 <mt2020.va@gmail.com>
2023-10-04 23:02:38 -0500

    APIs

---
+++ netflix-clone/src/components/Layout/index.jsx
@@ -0,0 +1,33 @@
+import logo from "../../assets/netflix-logo.png";
+import { searchInNetflix } from "../../services";
+
+export default function Layout({
+  children,
+  searchText,
+  setSearchText,
+  setTitles,
+}) {
+  const handleKeyUp = async (e) => {
+    if (e.key === "Enter" && searchText) {
+      const results = await searchInNetflix(searchText);
+      setTitles(results.titles);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex justify-between items-center ">
+        <img className="w-28 md:w-44 -ml-2 md:-ml-4" src={logo} alt="" />
+        <input
+          type="text"
+          placeholder="Buscar"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          onKeyUp={handleKeyUp}
+          className="py-1 px-2 outline-none bg-black/30 border"
+        />
+      </div>
+      {children}
+    </>
+  );
+}
